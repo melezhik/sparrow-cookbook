8514afb349bf2d33a866a2789d25cfd34f06fac5
Alexey Melezhik <melezhik@gmail.com>
2016-01-14 13:30:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Synopsis
+
+Installs [Sparrow](https://github.com/melezhik/sparrow) tool chain 
+
+
+# Requirements
+
+* perl
+* curl
+
+
+# Authors
+
+Alexey Melezhik
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "sparrow"
+maintainer       "Alexey Melezhik"
+maintainer_email "melezhik@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures Sparrow Tool Chain"
+version          "0.0.1"
+
+depends "perl"
+
